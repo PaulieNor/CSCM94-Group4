1950b94307f5c449c6d550adc7bcc80749ce431a
PaulieNor <64064864+PaulieNor@users.noreply.github.com>
2022-03-28 14:19:56 +0100

    Update MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -1,20 +1,25 @@
 public class MenuItem{
     //Constructor
     //Logistics
-    String NAME; //The name of the item
-    int PRICE; //The price in pence
+    private String NAME; //The name of the item
+    private int PRICE; //The price in pence
 
     //Dietary preference
-    boolean CONTAINS_MEAT; //Whether the product contains meat- for vegetarians
-    boolean ANIMAL_PRODUCT; //Whether the product is any kind of animal product- for vegans
-    boolean IS_HALAL; //Whether the product can be consumed by practicing Muslims
+    private boolean CONTAINS_MEAT; //Whether the product contains meat- for vegetarians
+    private boolean ANIMAL_PRODUCT; //Whether the product is any kind of animal product- for vegans
+    private boolean IS_HALAL; //Whether the product can be consumed by practicing Muslims
 
-    public MenuItem(String NAME, int PRICE, boolean CONTAINS_MEAT, boolean ANIMAL_PRODUCT, boolean IS_HALAL){
+    //Daily special
+    private boolean IS_SPECIAL;
+
+    public MenuItem(String NAME, int PRICE, boolean CONTAINS_MEAT,
+                    boolean ANIMAL_PRODUCT, boolean IS_HALAL, boolean IS_SPECIAL = false){
         this.NAME = NAME;
         this.PRICE = PRICE;
         this.CONTAINS_MEAT = CONTAINS_MEAT;
         this.ANIMAL_PRODUCT = ANIMAL_PRODUCT;
         this.IS_HALAL = IS_HALAL;
+        this.boolean IS_SPECIAL = IS_SPECIAL;
     }
 
     //Setters
@@ -38,6 +43,10 @@ public class MenuItem{
         this.IS_HALAL = NEW_IS_HALAL;
     }
 
+    public void setSpecial(boolean IS_SPECIAL){
+        this.IS_SPECIAL = NEW_IS_SPECIAL;
+    }
+
     //Getters
     public String getName(){
         return this.NAME;
@@ -57,6 +66,10 @@ public class MenuItem{
 
     public boolean getIsHalal(){
         return this.IS_HALAL;
+    }
+
+    public boolean getIsSpecial(){
+        return this.IS_Special;
     }
 
 }
