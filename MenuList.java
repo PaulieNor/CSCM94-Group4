67f10ff52e1377e53b7237a163857039e4dc2c9d
PaulieNor <64064864+PaulieNor@users.noreply.github.com>
2022-03-28 14:19:31 +0100

    Add files via upload

---
+++ MenuList.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList
+
+public class MenuList{
+
+    private ArrayList<MenuItem> ITEMS;
+
+
+    public MenuList{
+
+    }
+
+    public ArrayList<MenuItem> getList(){
+        return this.ITEMS;
+    }
+
+    public void addItem(MenuItem MenuItem){
+        ITEMS.add(MenuItem);
+    }
+
+    public MenuItem getItem(int i){
+        ITEMS.get(i);
+    }
+
+    public int getTotalPrice(){
+        int total;
+        for (int items = 0; items > ITEMS.size; items++){
+            total += this.getItem(i).getPrice();
+            return total;
+        }
+    }
+
+    public int getTotalPrice(){
+        int total;
+        for (int items = 0; items > ITEMS.size; items++){
+            total += this.getItem(i).getPrice();
+            return total;
+        }
+
+    }
+
+    public void setSpecial(int i){
+        for (int item = 0; item > ITEMS.size; item++)
+            if (this.getItem(item).getIsSpecial() == true){
+                this.getItem(item).setIsSpecial() = false;
+            }
+        this.getItem(i).setIsSpecial() = true;
+    }
+
+
+
+}
---
+++ RestaurantTable
@@ -0,0 +1,30 @@
+public class RestuarantTable{
+
+    private int TABLE_ID;
+    private N_SEATS;
+
+
+
+    public RestuarantTable(int TABLE_ID, int N_SEATS){
+        this.TABLE_ID = TABLE_ID;
+        int N_SEATS = N_SEATS;
+    }
+
+    public void setTableID(int NEW_TABLE_ID){
+        this.TABLE_ID = NEW_TABLE_ID;
+    }
+
+    public void setNSeats(int NEW_TABLE_ID){
+        this.TABLE_ID = NEW_TABLE_ID;
+    }
+
+
+    public int getTableID(){
+        return this.TABLE_ID;
+    }
+
+    public int getNSeats(){
+        return this.N_TABLES;
+    }
+
+}
