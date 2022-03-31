98d5d00777588a0332cd2ca7cfb9667e0391166b
RyanNoscoe <99408961+RyanNoscoe@users.noreply.github.com>
2022-03-31 12:55:16 +0100

    Add files via upload

---
+++ Task2/source/Cafe94/Cafe94/README.txt
@@ -0,0 +1,28 @@
+[FILES]------------------------------------
+- Main.java
+- Controller.java
+- ManageStaff.java
+
+- Manager.fxml
+- ManageStaff.fxml
+- StaffLogin.fxml
+
+- module-info.java
+- managestaff.sql
+- mysql-connector-java-8.0.28
+
+[LOCATIONS]--------------------------------
+Main .JAVAfiles:
+Cafe94\src\main\java\cscm12\cafe94
+
+Main .FXML files:
+Cafe94\src\main\resources\cscm12\cafe94
+
+Database, SQL connector, Module info:
+Cafe94\src\main\java
+
+Other files (JDK generated):
+Cafe94\target\classes\cscm12\cafe94
+Cafe94\target\classes
+
+^ Names for these are the same as .java and .fxml's.
---
+++ Task2/source/Cafe94/Cafe94/src/main/java/cscm12/cafe94/Main.java
@@ -0,0 +1,39 @@
+package cscm12.cafe94;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Objects;
+
+/**
+ * [Main]
+ * Main place where the stage starts to branch out, the login page (root).
+ *
+ * @author Sumi Sunuwar
+ * @version 1.0
+ */
+public class Main extends Application {
+
+    /**
+     * [start]
+     * Main place stage, this is used as the beginning page.
+     * Can be used to plug in the direct FXML page if you dont want to login all the time.
+     */
+    @Override
+    public void start(Stage stage){
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("StaffLogin.fxml")));
+            Scene scene = new Scene(root);
+            stage.setTitle("Cafe94");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
