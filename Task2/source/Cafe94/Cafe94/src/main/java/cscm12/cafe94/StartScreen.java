96d75410e49305f4a71365b8ef3c9c0a25187ae3
RyanNoscoe <99408961+RyanNoscoe@users.noreply.github.com>
2022-04-02 17:14:46 +0100

    New Classes to hold the switch methods for FXML files

---
+++ Task2/source/Cafe94/Cafe94/src/main/java/cscm12/cafe94/StartScreen.java
@@ -0,0 +1,44 @@
+package cscm12.cafe94;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class StartScreen {
+    /**[Field Variables]
+     * These are for setting up stages to be displayed in the application. */
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    /**   [switchToStaffLogin]
+     Switches to staffs login page.
+     @param event triggers button to go to the fxml upon clicking. */
+    @FXML
+    public void switchToStaffLogin(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("StaffLogin.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    /**   [switchToOrderType]
+     Switches to page where customers select their order type.
+     @param event triggers button to go to the fxml upon clicking. */
+    @FXML
+    public void switchToOrderType(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("OrderType.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
