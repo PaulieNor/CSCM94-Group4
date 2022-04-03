d1fd4cba3bf8834ee697e108d2c187aa7e7e02c4
PaulieNor <64064864+PaulieNor@users.noreply.github.com>
2022-04-03 09:29:21 +0100

    adding the database() here just for convenience, gonna phase it out properly in a bit.

--- Task2/source/Cafe94/Cafe94/src/main/java/cscm12/cafe94/DBConnector.java
+++ Task2/source/Cafe94/Cafe94/src/main/java/cscm12/cafe94/DBConnector.java
@@ -14,21 +14,10 @@ import java.sql.*;
 public class DBConnector extends DatabaseHandler{
 
 
+    private String placeholder;
+
     public static Connection getConnection() throws SQLException {
-        Connection database = null;
-        try {
-            String url = "jdbc:sqlserver://cafe94.database.windows.net:1433;"
-                    + "database=cafe94;"
-                    + "user=adminCafe@cafe94;"
-                    + "password=cscm94Group4;"
-                    + "encrypt=true;"
-                    + "trustServerCertificate=true;"
-                    + "loginTimeout=30;";
-            Connection con = DriverManager.getConnection(url);
-            return con;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        return database();
         }
     }
 
