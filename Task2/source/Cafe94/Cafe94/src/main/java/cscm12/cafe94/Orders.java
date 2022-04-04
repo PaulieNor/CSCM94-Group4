da8fa3fc49f3ac78d1d64e62a0ffdaa0a7ac067c
patrickrosecode <87778782+patrickrosecode@users.noreply.github.com>
2022-04-04 20:27:37 +0100

    Redo of Orders Class to create OrdersController

--- Task2/source/Cafe94/Cafe94/src/main/java/cscm12/cafe94/Orders.java
+++ Task2/source/Cafe94/Cafe94/src/main/java/cscm12/cafe94/Orders.java
@@ -1,7 +1,20 @@
 package cscm12.cafe94;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import java.net.URL;
 import java.util.*;
 import java.io.IOException;
 import java.sql.Connection;
@@ -14,40 +27,23 @@ import java.sql.Statement;
  * @author Patrick Rose
  * @version 1.0
  */
-public class Orders {
-    public Orders(int main, int side, int drink, String order_type, int reference_number) {
+
+public class Orders{
+
+    private SimpleStringProperty main;
+    private SimpleStringProperty side;
+    private SimpleStringProperty drink;
+    private SimpleStringProperty orderTypes;
+    private SimpleIntegerProperty referenceNumber;
+
+    public Orders(String main, String side, String drink, String orderTypes, int referenceNumber) {
+        this.main = new SimpleStringProperty(main);
+        this.side = new SimpleStringProperty(side);
+        this.drink = new SimpleStringProperty(drink);
+        this.orderTypes = new SimpleStringProperty(orderTypes);
+        this.referenceNumber = new SimpleIntegerProperty(referenceNumber);
     }
-    public static void main(String[] args) {
-    }
-    /**
-     * [showIncomplete]
-     * Gets all orders that are yet to be completed to display to the chef.
-     */
-    public static ObservableList<Orders> showIncomplete() {
-        DatabaseHandler handler = new DatabaseHandler();
-        Connection connect = handler.database();
-        ObservableList<Orders> outstandingOrders =  FXCollections.observableArrayList();
-        try {
-            Statement statement = connect.createStatement();
-            String sql = "SELECT * FROM vMasterOrderSheet WHERE IsCompleted = '0'";
-            PreparedStatement checkDatabase = connect.prepareStatement(sql);
-            ResultSet resultSet = checkDatabase.executeQuery();
-            Orders orders;
-            while (resultSet.next()){
-                orders = new Orders(resultSet.getInt("Main"),
-                                    resultSet.getInt("Side"),
-                                    resultSet.getInt("Drink"),
-                                    resultSet.getString("order_type"),
-                                    resultSet.getInt("reference_number"));
-                    outstandingOrders.add(orders);
-            }
-            statement.close();
-            connect.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return outstandingOrders;
-    }
+
     /**
      * [orderTotal]
      * Method to return the total price of an order.
@@ -55,7 +51,8 @@ public class Orders {
      * @param orderType
      * @return total
      */
-    public static double orderTotal(int referenceNumber, String orderType) {
+    /**
+    public static double getOrderTotal(int referenceNumber, String orderType) {
         DatabaseHandler handler = new DatabaseHandler();
         Connection connect = handler.database();
         double total = 0.0;
@@ -71,4 +68,66 @@ public class Orders {
         }
         return total;
     }
+     */
+
+    public String getMain() {
+        return main.get();
+    }
+
+    public void setMain(String aMain) {
+        main.set(aMain);
+    }
+
+    public SimpleStringProperty mainProperty() {
+        return main;
+    }
+
+    public String side() {
+        return side.get();
+    }
+
+    public void setSide(String aSide) {
+        side.set(aSide);
+    }
+
+    public SimpleStringProperty sideProperty() {
+        return side;
+    }
+
+    public String drink() {
+        return drink.get();
+    }
+
+    public void setDrink(String aDrink) {
+        drink.set(aDrink);
+    }
+
+    public SimpleStringProperty drinkProperty() {
+        return drink;
+    }
+
+    public String orderTypes() {
+        return orderTypes.get();
+    }
+
+    public void setOrderTypes(String anOrderTypes) {
+        orderTypes.set(anOrderTypes);
+    }
+
+    public SimpleStringProperty orderTypesProperty() {
+        return orderTypes;
+    }
+
+    public int referenceNumber() {
+        return referenceNumber.get();
+    }
+
+    public void setReferenceNumber(int aReferenceNumber) {
+        referenceNumber.set(aReferenceNumber);
+    }
+
+    public SimpleIntegerProperty referenceNumberProperty() {
+        return referenceNumber;
+    }
 }
+
